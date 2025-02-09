09fc16ea28c2d1efd7cdeb9f6434f27779655fb7
Kaylahray <chiomachristopher91@gmail.com>
2025-02-09 16:42:10 +0100

    setting up App wallet provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// pages/_app.tsx or app/layout.tsx
+import "@solana/wallet-adapter-react-ui/styles.css";
+import AppWalletProvider from "@/components/AppWalletProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AppWalletProvider>{children}</AppWalletProvider>
       </body>
     </html>
   );
---
+++ src/components/AppWalletProvider.tsx
@@ -0,0 +1,39 @@
+"use client";
+// require("@solana/wallet-adapter-react-ui/styles.css");
+
+import React, { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
+// import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+// Default styles that can be overridden by your app
+// imports here
+export default function AppWalletProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+    () => [
+      // manually add any legacy wallet adapters here
+      // new UnsafeBurnerWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
